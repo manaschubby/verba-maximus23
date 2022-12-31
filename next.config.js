8869aba0b66d0193134a5750cb3b48900ec17ac4
manaschubby <manaschubby@gmail.com>
2022-12-31 18:28:30 +0530

    Basic Router Functionality

--- next.config.js
+++ next.config.js
@@ -5,3 +5,13 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/:any*',
+        destination: '/',
+      },
+    ];
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { useEffect, useState } from 'react';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [windowAvailable, setWindow] = useState(true)
+  useEffect(() => setWindow(false));
+  return (
+    <div suppressHydrationWarning>
+      {windowAvailable ? null : <Component {...pageProps} />}
+    </div>
+  );
 }
 
 export default MyApp
